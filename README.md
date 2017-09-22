df0a9fd63ca299062325259e262f8b817e53b4fa
velios <prostozapomnit@gmail.com>
2017-09-22 19:31:20 +0300

    Fix inspector comments
    Simplified code base
    Rewrite all functions
    Fix indents

--- README.md
+++ README.md
@@ -14,12 +14,9 @@ pip install -r requirements.txt # alternatively try pip3
 ##### Sample run
 ```bash
 $ python check_sites_health.py urls.txt
-Users who sent tasks between 04:00:00 and 08:00:00:
-  1 pikabu.ru     Status code 200 OK: PASSED | Expiration date more 30 days: PASSED | 196 days left
-  2 habr.ru       Status code 200 OK: FAILED | Expiration date more 30 days: PASSED | 210 days left
-  3 facebook.com  Status code 200 OK: PASSED | Expiration date more 30 days: PASSED | 2749 days left
-  4 twitter.com   Status code 200 OK: PASSED | Expiration date more 30 days: PASSED | 854 days left
-  5 google.com    Status code 200 OK: FAILED | Expiration date more 30 days: PASSED | 1091 days left
+Checked url info:
+Url https://devman.org... 200 OK checked: PASSED Expiration less than 30 days: PASSED
+Url https://slack.com... 200 OK checked: PASSED Expiration less than 30 days: PASSED
 ```
 
 # Project Goals
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,68 +1,59 @@
-import logging
 from datetime import datetime
-from argparse import ArgumentParser
-from contextlib import suppress
+from argparse import ArgumentParser, FileType
+from requests.exceptions import MissingSchema, ConnectionError
 
 import whois
 import requests
-import validators
 
 
 def make_cmd_arguments_parser():
     parser_description = 'Scripts check response status and expiration date in urls from text file'
     parser = ArgumentParser(description=parser_description)
     parser.add_argument('file_path',
-                        help='File with urls')
+                        help='File with urls',
+                        type=FileType('r', encoding='UTF-8'))
     return parser.parse_args()
 
 
-def load_urls4check_generator(file_path):
-    with open(file_path, 'r') as file:
+def make_urls4check_generator(urls_file):
+    with urls_file as file:
         for line in file:
-            yield line.rstrip()
+            if line.rstrip():
+                yield line.rstrip()
 
 
-def is_server_respond_with_200(domain):
-        checked_protocols = ['http://', 'https://', 'http://www.', 'https://www.']
-        with suppress(BaseException):
-            for protocol in checked_protocols:
-                with requests.head('{}{}'.format(protocol, domain)) as request:
-                    if request.status_code == requests.codes.ok:
-                        return True
+def is_server_respond_with_200(url):
+    with requests.head('{}'.format(url)) as request:
+            return request.status_code == requests.codes.ok
+
+
+def get_whois_info(url):
+    return whois.whois(url)
+
+
+def is_whois_expiration_date_less_n_days(url, days=30):
+    whois_info = get_whois_info(url)
+    whois_expiration_date = whois_info.expiration_date[0]
+    today = datetime.now()
+    remaining_paid_period_in_days = (whois_expiration_date - today).days
+    return remaining_paid_period_in_days > days
 
 
 if __name__ == '__main__':
     cmd_arguments = make_cmd_arguments_parser()
-    file_path = cmd_arguments.file_path
-    today = datetime.now()
-    for index, domain in enumerate(load_urls4check_generator(file_path)):
-        index += 1
+    urls_file = cmd_arguments.file_path
+    checked_urls_generator = make_urls4check_generator(urls_file)
+    print('Checked url info:')
+    for url in checked_urls_generator:
         try:
-            if validators.url(domain):
-                pass
-            elif validators.domain(domain):
-                url = 'www.{}'.format(domain)
-            else:
-                print('{:3} {:24} is not correct data'.format(index, domain))
-                continue
-            whois_data = whois.whois(domain)
-            if not whois_data.domain_name:
-                print('{:3} {:24} is not found in whois'.format(index, url))
-                continue
-        except (ValueError, ConnectionError, ConnectionRefusedError) as err:
-            logging.warning('{:3} {:24}'.format(index, err))
-        finally:
-            site_status = 'PASSED' if is_server_respond_with_200(domain) else 'FAILED'
-            site_expiration_date = whois_data.expiration_date[0] if isinstance(whois_data.expiration_date, list) else whois_data.expiration_date
-            with suppress(TypeError):
-                site_expiration_advice, site_expiration_timedelta = None, None
-                if site_expiration_date:
-                    site_expiration_timedelta = site_expiration_date - today
-                    site_expiration_advice = 'PASSED' if site_expiration_timedelta.days > 30 else 'FAILED'
-            print('{:3} {:24} Status code 200 OK: {} | Expiration date more 30 days: {} | {} days left'.format(
-                index,
-                domain,
-                site_status,
-                site_expiration_advice if site_expiration_advice else 'UNKNOWN',
-                site_expiration_timedelta.days if site_expiration_timedelta else 'UNKNOWN',
-            ))
+            is_respond_200 = 'PASSED' if is_server_respond_with_200(url) else 'FAILED'
+            is_expiration_less_30_days = 'PASSED' if is_whois_expiration_date_less_n_days(url) else 'FAILED'
+        except (MissingSchema, ConnectionError, TypeError) as err:
+            print('Error: ', err)
+        else:
+            print(
+                'Url {}... 200 OK checked: {} '
+                'Expiration less than 30 days: {}'.format(url,
+                                                          is_respond_200,
+                                                          is_expiration_less_30_days)
+            )
